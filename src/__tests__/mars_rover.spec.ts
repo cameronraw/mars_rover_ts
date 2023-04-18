13d58079be654fc78b6723ab4df1ed5a0b56f9c1
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-04-18 12:08:53 +0200

    Finished version - no patterns

--- src/__tests__/mars_rover.spec.ts
+++ src/__tests__/mars_rover.spec.ts
@@ -1,37 +1,5 @@
 import MarsRover from "../mars_rover";
 
-// Step 1
-
-describe("Mars Rover should", () => {
-	it("return '0:0:W' when sent 'L'", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("L");
-		expect(response).toBe("0:0:W");
-	});
-});
-
-// Step 2
-
-describe("Mars Rover should", () => {
-	it("return '0:0:W' when sent 'L'", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("L");
-		expect(response).toBe("0:0:W");
-	});
-	it("return '0:0:S' when sent 'LL'", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("LL");
-		expect(response).toBe("0:0:S");
-	});
-	it("return '0:0:S' when sent 'LLL'", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("LLL");
-		expect(response).toBe("0:0:E");
-	});
-});
-
-
-// Once we have all the tests, we can refactor into parameterized tests, etc..
 describe("Mars Rover should", () => {
 	it.each([
 		["0:0:W", "L"],
@@ -42,26 +10,20 @@ describe("Mars Rover should", () => {
 		["0:0:S", "RR"],
 		["0:0:W", "RRR"],
 		["0:0:N", "RRRR"],
+		["0:1:N", "M"],
+		["0:2:N", "MM"],
+		["0:0:N", "MMMMMMMMMM"],
+		["1:0:E", "RM"],
+		["1:0:E", "RMMMMMMMMMMM"],
+		["9:0:W", "LM"],
+		["8:0:W", "LMM"],
+		["0:0:W", "LMMMMMMMMMM"],
+		["9:0:W", "LMMMMMMMMMMM"],
+		["0:9:S", "LLM"],
+		["0:9:S", "LLMMMMMMMMMMM"],
 	])("return %p when sent command %p", (expectedResponse, command) => {
 		const marsRover = new MarsRover();
 		const response = marsRover.execute(command);
 		expect(response).toBe(expectedResponse);
 	});
 });
-
-// Movement
-
-describe("Mars Rover should", () => {
-	it("return '0:1:N' when sent 'M' whilst facing North", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("M");
-		expect(response).toBe("0:1:N");
-	});
-	it("return '1:0:W' when sent 'M' whilst facing West", () => {
-		const marsRover = new MarsRover();
-		const response = marsRover.execute("RM");
-		expect(response).toBe("1:0:W");
-	});
-});
-
-
--- src/mars_rover.ts
+++ src/mars_rover.ts
@@ -1,24 +1,57 @@
 export default class MarsRover {
 
   private orientation: string = 'N';
+  private y: number = 0;
+  private x: number = 0;
 
   execute(commandString: string) {
 
     commandString.split("").forEach(char => {
-      if(char === "L"){
-        this.moveLeft();
-      };
-
-      if(char === "R"){
-        this.moveRight();
-      };
+      this._executeCommand(char);
     });
 
-    return `0:0:${this.orientation}`;
+    return this.buildOutput();
 
   }
-  
-  private moveRight() {
+
+  private buildOutput() {
+    return `${this.x}:${this.y}:${this.orientation}`;
+  }
+
+  private _executeCommand(char: string) {
+    if (char === "L") {
+      this._rotateLeft();
+    }
+    ;
+
+    if (char === "R") {
+      this._rotateRight();
+    }
+    ;
+
+    if (char === "M") {
+      this._moveForward();
+    }
+  }
+
+  private _moveForward() {
+    switch (this.orientation) {
+      case "N":
+        this.y = this.y < 9 ? this.y + 1 : 0;
+        break;
+      case "E":
+        this.x = this.x < 9 ? this.x + 1 : 0;
+        break;
+      case "W":
+        this.x = this.x === 0 ? 9 : this.x - 1;
+        break;
+      case "S":
+        this.y = this.y === 0 ? 9 : this.y - 1;
+        break;
+    }
+  }
+
+  private _rotateRight() {
     switch(this.orientation){
       case 'N':
         this.orientation = 'E';
@@ -37,7 +70,7 @@ export default class MarsRover {
     }
   };
 
-  private moveLeft() {
+  private _rotateLeft() {
     switch(this.orientation){
       case 'N':
         this.orientation = 'W';
